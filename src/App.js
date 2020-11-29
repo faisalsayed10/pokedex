c0130b5a7c3eef2758b10b72e3ffbe143c57afba
faisalsayed10 <faisal.sayed502@gmail.com>
2020-11-29 11:12:24 +0530

    data fetching successful

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
-import useSwr from "swr";
+import "./styles.css";
+import Pokemon from "./components/Pokemon";
+import { useRequest } from "./useRequest";
 
 function App() {
+  const { data, error } = useRequest("");
+
+  if (error) return <h1>Something went wrong!</h1>;
+  if (!data) return <h1>Loading...</h1>;
+
   return (
-    <div className="App">
-    </div>
+    <main className="App">
+      <h1>Pokedex</h1>
+      <div>
+        {data.results.map((pokemon) => (
+          <Pokemon key={pokemon.name} pokemon={pokemon} />
+        ))}
+      </div>
+    </main>
   );
 }
 
